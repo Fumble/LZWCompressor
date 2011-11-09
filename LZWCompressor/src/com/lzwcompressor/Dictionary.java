7a80049bfae33a842ffa00d039ec12687eb2d6fa
Fumble <fumblefr@gmail.com>
2011-11-09 10:32:54 +0100

    Added possibility to retrieve the key from a value.

--- LZWCompressor/src/com/lzwcompressor/Dictionary.java
+++ LZWCompressor/src/com/lzwcompressor/Dictionary.java
@@ -3,18 +3,19 @@ package com.lzwcompressor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 
 public class Dictionary {
 	private TreeMap<Integer, String> dico;
 	private int index;
-	
-	public Dictionary(){
+
+	public Dictionary() {
 		dico = new TreeMap<Integer, String>();
 		setIndex(0);
 	}
-	
+
 	public static String readFileAsString(String filePath)
 			throws java.io.IOException {
 		StringBuilder fileData = new StringBuilder();
@@ -27,31 +28,42 @@ public class Dictionary {
 		reader.close();
 		return fileData.toString();
 	}
-	
-	public void display()
-	{
+
+	public void display() {
 		Set<Integer> keySet = dico.keySet();
 		Iterator<Integer> it = keySet.iterator();
 		int nomCurr;
-		while(it.hasNext())
-		{
+		while (it.hasNext()) {
 			nomCurr = it.next();
-			System.out.println(nomCurr+" ==> "+dico.get(nomCurr));
+			System.out.println(nomCurr + " ==> " + dico.get(nomCurr));
 		}
 	}
-	
-	public void init(){
-		for(char i = 0; i<256; i++){
-			dico.put((int)i, String.valueOf(i));
+
+	public void init() {
+		for (char i = 0; i < 256; i++) {
+			dico.put((int) i, String.valueOf(i));
 		}
 		setIndex(256);
 	}
-	
-	public String getValue(int key){
+
+	public String getValue(int key) {
 		return dico.get(key);
 	}
-	
-	public void put(int key, String value){
+
+	public int getKey(String value) {
+		Set<Entry<Integer, String>> entryset = dico.entrySet();
+		Iterator<Entry<Integer, String>> it = entryset.iterator();
+		int res = -1;
+		while (it.hasNext()) {
+			Entry<Integer, String> elem = it.next();
+			if (elem.getValue().equals(value)) {
+				res = elem.getKey();
+			}
+		}
+		return res;
+	}
+
+	public void put(int key, String value) {
 		dico.put(key, value);
 	}
 
@@ -62,14 +74,13 @@ public class Dictionary {
 	public void setIndex(int index) {
 		this.index = index;
 	}
-	
-	public boolean containsValue(String value){
+
+	public boolean containsValue(String value) {
 		return dico.containsValue(value);
 	}
-	
-	public boolean containsKey(int key){
+
+	public boolean containsKey(int key) {
 		return dico.containsKey(key);
 	}
-
 
 }
