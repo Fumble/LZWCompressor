a48c60cbb188945d8d6e58e2d13bb2b03888857c
Fumble <fumblefr@gmail.com>
2011-11-16 12:00:01 +0100

    Formatted source. Minor tweaks and fixes.

--- LZWCompressor/src/com/lzwcompressor/LZWTest.java
+++ LZWCompressor/src/com/lzwcompressor/LZWTest.java
@@ -12,31 +12,32 @@ public class LZWTest {
 	private Dictionary dicoCompression;
 	private Dictionary dicoDecompression;
 	private int numBits;
-	
-	public LZWTest(){
+
+	public LZWTest() {
 		numBits = 12;
 	}
-	
-	public LZWTest(int numBits){
+
+	public LZWTest(int numBits) {
 		this.numBits = numBits;
 	}
-	
-    private File file ;
-    private FileInputStream file_input;
-    private DataInputStream data_input ;
+
+	private File file;
+	private FileInputStream file_input;
+	private DataInputStream data_input;
+
 	/**
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		//String s = "tobeornottobeortobeornot";
-		String s ="Loremipsumdolorsitamet,consecteturadipiscingelit.Fuscenecturpisquisnequepulvinarporttitoracatodio.";
-		LZWTest lzw = new LZWTest();
+		String s = "tobeornottobeortobeornot";
+		//String s = "Loremipsumdolorsitamet,consecteturadipiscingelit.Fuscenecturpisquisnequepdldgjskqdndsglqshdfdkfnqsifhdkflsdgiuqsjdkosidghjqspofhdfsoflqsjfiudsfhqsfghdsfiolulvinarporttitoracatodio.";
+		LZWTest lzw = new LZWTest(12);
 		lzw.compression(s);
 	}
 
 	public void compression(String s) throws IOException {
-		dicoCompression = new Dictionary(1<<numBits);
+		dicoCompression = new Dictionary(1 << numBits);
 		dicoCompression.init();
 		String w = "";
 		char c;
@@ -52,7 +53,7 @@ public class LZWTest {
 				dicoCompression.put(255 + (i - ((w + c).length() - 2)), w + c);
 				if (dicoCompression.getKey(w) <= 255) {
 					System.out.println(w);
-					os.writeByte(w.charAt(0));
+					os.write(w.charAt(0));
 				} else {
 					System.out.println(dicoCompression.getKey(w));
 					// System.out.println(w);
@@ -70,34 +71,32 @@ public class LZWTest {
 	}
 
 	public void decompression(String filename) {
-        Short code = null;
-        String c = null,w = null, entree = null;
-        
-        
-        file=new File(filename);
-        try {
+		Short code = null;
+		String c = null, w = null, entree = null;
+
+		file = new File(filename);
+		try {
 			file_input = new FileInputStream(file);
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
-        data_input    = new DataInputStream(file_input);
-        
-        try {
+		data_input = new DataInputStream(file_input);
+
+		try {
 			code = data_input.readShort();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-        c= dicoCompression.getValue(code);
-        
+
+		c = dicoCompression.getValue(code);
+
 		int i = 1;
 		w = c;
 		dicoDecompression = new Dictionary();
 		while (code != -1) {
 			if (code > 255 && dicoDecompression.containsKey(code)) {
 				entree = w;
-			} else if (code > 255
-					&& !dicoDecompression.containsKey(code)) {
+			} else if (code > 255 && !dicoDecompression.containsKey(code)) {
 				entree = w + w.charAt(0);
 			} else {
 				entree = c;
@@ -106,16 +105,15 @@ public class LZWTest {
 			dicoDecompression.put(255 + i, w + entree.charAt(0));
 			w = entree;
 			i++;
-			c= dicoCompression.getValue(code);
+			c = dicoCompression.getValue(code);
 		}
 	}
 
-
 	void writeCode(DataOutputStream os, int code) throws IOException {
-//		System.out.println("BIT TO WRITE "
-//				+ (32 - Integer.numberOfLeadingZeros(code)));
-		for (int i = 0; i < (32 - Integer.numberOfLeadingZeros(code)); ++i) {
-			os.write(code & 1);
+		// System.out.println("BIT TO WRITE "
+		// + (32 - Integer.numberOfLeadingZeros(code)));
+		for (int i = 0; i < (32 - Integer.numberOfLeadingZeros(code)); i++) {
+			os.write(code&1);
 			code /= 2;
 		}
 	}
